66d57c366fa8465446d28643d29c6eade619eb50
retro <retro@retros-mbp.home>
2022-07-11 17:39:15 +0100

    add some libcd funcs

--- include/libcd.h
+++ include/libcd.h
@@ -1,11 +1,30 @@
 #ifndef GAME_LIBCD_H
 #define GAME_LIBCD_H
 
+typedef void (*CdlCB)(unsigned char,unsigned char *);
+
+/*
+    Structs
+*/
+
+typedef struct {
+	unsigned char minute;		/* minute (BCD) */
+	unsigned char second;		/* second (BCD) */
+	unsigned char sector;		/* sector (BCD) */
+	unsigned char track;		/* track (void) */
+} CdlLOC;
+
 /*
     Functions
 */
 
 int LIBCD_CdRead(int sectors, unsigned int * buf, int mode);
 int LIBCD_CdInit();
+void (*LIBCD_CdDataCallback(void (*func)()));
+int LIBCD_CdStatus(void);
+int LIBCD_CdControl(unsigned char com, unsigned char *param, unsigned char *result);
+int LIBCD_CdControlB(unsigned char com, unsigned char *param, unsigned char *result);
+CdlCB LIBCD_CdReadCallback(CdlCB func);
+CdlLOC *LIBCD_CdIntToPos(int i, CdlLOC *p) ;
 
 #endif
