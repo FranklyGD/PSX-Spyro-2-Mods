bcc3687a836d5673645ab957a6570959312ecf35
mateusfavarin <mateusfavarinc@gmail.com>
2022-07-11 00:18:39 -0300

    Rename misleading function name

--- include/common.h
+++ include/common.h
@@ -26,7 +26,8 @@ typedef struct {
 
 void GAME_main();
 void GAME_DrawText(char * text, int x, int y, int colorIndex, TextConfig * textConfig);
-void GAME_DrawTransparentFlatRect(short x1, short x2, short y1, short y2);
+// Semi-transparent, Flat, RGB = (8, 8, 8)
+void GAME_DrawBlackRect(short x1, short x2, short y1, short y2);
 void GAME_memcpyWord(int *dst,int *src,int size);
 void GAME_memsetWord(int *buffer,int n,int size);
 
