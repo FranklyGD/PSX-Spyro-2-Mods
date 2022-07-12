fc58209e6a7ec6cb810f25f083f9dbfa8308ce82
mateusfavarin <mateusfavarinc@gmail.com>
2022-07-12 02:10:44 -0300

    Add spyro data

--- include/vec3.h
+++ include/vec3.h
@@ -1,10 +1,29 @@
 #ifndef GAME_VEC3_H
 #define GAME_VEC3_H
 
-void GAME_Vec3_Null(int * v);
-void GAME_Vec3_Move(int * dst, int * src);
-void GAME_Vec3_Sum(int * dst, int * v1, int * v2);
-void GAME_Vec3_Sub(int * dst, int * v1, int * v2);
-void GAME_Vec3_DivByN(int * dst, int * v, int n);
+/*
+    Structs
+*/
+
+typedef struct {
+    int x;
+    int z;
+    int y;
+} Vec3;
+
+/*
+    Functions
+*/
+
+// v = 0
+void GAME_Vec3_Null(Vec3 * v);
+// dst = src
+void GAME_Vec3_Move(Vec3 * dst, Vec3 * src);
+// dst = v1 + v2
+void GAME_Vec3_Sum(Vec3 * dst, Vec3 * v1, Vec3 * v2);
+// dst = v1 - v2
+void GAME_Vec3_Sub(Vec3 * dst, Vec3 * v1, Vec3 * v2);
+// dst = v / n
+void GAME_Vec3_DivByN(Vec3 * dst, Vec3 * v, int n);
 
 #endif
