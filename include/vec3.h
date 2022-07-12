b8b1ef1f4cb672a1ec19b2c24ebe347be5aa02e5
mateusfavarin <mateusfavarinc@gmail.com>
2022-07-12 02:56:28 -0300

    Stub more Vec3 functions

--- include/vec3.h
+++ include/vec3.h
@@ -15,6 +15,12 @@ typedef struct {
     Functions
 */
 
+// v = v * n / m
+void GAME_Vec3_Scale(Vec3 * v, int m, int n);
+// v = v << (n & 0x1F)
+void GAME_Vec3_LShift(Vec3 * v, int n);
+// v = v >> (n & 0x1F)
+void GAME_Vec3_RShift(Vec3 * v, int n);
 // v = 0
 void GAME_Vec3_Null(Vec3 * v);
 // dst = src
@@ -23,6 +29,8 @@ void GAME_Vec3_Move(Vec3 * dst, Vec3 * src);
 void GAME_Vec3_Sum(Vec3 * dst, Vec3 * v1, Vec3 * v2);
 // dst = v1 - v2
 void GAME_Vec3_Sub(Vec3 * dst, Vec3 * v1, Vec3 * v2);
+// dst = v * n
+void GAME_Vec3_MultByN(Vec3 * dst, Vec3 * v, int n);
 // dst = v / n
 void GAME_Vec3_DivByN(Vec3 * dst, Vec3 * v, int n);
 
